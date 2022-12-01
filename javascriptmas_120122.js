c391377080671dea9c42f413651b1d28b4fcb190
bribricot <kraemer.lucie@gmail.com>
2022-12-01 23:56:54 +0100

    Return values, variable actualisation, arguments reusable and optimization through direct return statements, join & split inbuild methods

--- javascriptmas_120122.js
+++ javascriptmas_120122.js
@@ -1,28 +1,33 @@
 
 /* Panic function 
-Write a PANIC! function. The function should take in a sentence and return the same
-sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
-built in string methods. 
+The function should take in a sentence and return the same
+sentence in all caps with an exclamation point (!) at the end. 
+Use JavaScript's built in string methods join & split.
 
 If the string is a phrase or sentence, add a 😱 emoji in between each word. 
-
-Example input: "Hello"
-Example output: "HELLO!"
-
-Example input: "I'm almost out of coffee"
-Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
-
-.split() .join()
 */
-
-function panic(input){
-    if (MSInputMethodContext) {
-        
-    }
-	return input.toUpperCase() + "!";
+function emojify(words) {
+	return words.join(" 😱 ");
 }
 
-// Test your function
-console.log(panic("I'm almost out of coffee")); 
-console.log(panic("winter is coming"))
+function capitalize(str){
+	return str.toUpperCase() + "!";
+}
+
+function panic(str) {
+	let words = str.split(" ");
+	if (words)
+		str = emojify(words);
+	str = capitalize(str);
+    return str;
+}
+
+// Testing
+console.log(panic("Hello"));
+console.log(panic("I'm almost out of coffee"));
+ 
+//If word only : Expected output: "HELLO!"
+//If sentence : Expected output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+
+
 
