5e31b33e8f2fc5dd3d4bea299d7e0cae2e583639
bribricot <kraemer.lucie@gmail.com>
2022-12-02 23:57:06 +0100

    Manipulation d'objet, tableau d'objets et push

---
+++ 12.1/papa.js
@@ -0,0 +1,14 @@
+// Split N Join
+
+let a = "bonjour je suis louinis"
+a = a.split(' ') // ["bonjour", "je", "suis", "louinis"]
+a.push("boudaa") // ["bonjour", "je", "suis", "louinis", "boudaa"]
+a = a.join(' ')
+
+// cinq types de trucs que tu peux rencontrer
+
+commentaires
+if else while return for
+"logdfddgd" 4553 {x:3, y:2, name:'salut'} [2, 0] true
+a, b, c, d, e, f, index, console, window
+log getelementbyid capitalize panic 
