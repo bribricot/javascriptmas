2f9accaeb27bb8aac7f50edfbe28b2b78ca17aec
bribricot <kraemer.lucie@gmail.com>
2022-12-04 19:51:07 +0100

    Transformation de string

--- 12.4/javascriptmas_120422.js
+++ 12.4/javascriptmas_120422.js
@@ -13,5 +13,31 @@ output: "shh... the kittens are sleeping"
 Hint: endsWith and slice
 */
 
-console.log(whisper("PLEASE STOP SHOUTING."));
-console.log(whisper("MA'AM, this is a Wendy's!"));
+//Si tab de string :
+function whisper(tab) {
+	for (let i = 0; i < tab.length; i++) {
+		let newstr = tab[i].toLowerCase();
+	    if (newstr.endsWith('!')) // Ou if newstr[newstr.length - 1] === '!';
+	  		newstr = newstr.slice(0, [-1]);
+	  	tab[i] = "shh... " + newstr;
+  	}
+  	return tab;
+}
+
+//Appeler plrs fois fn
+for (let j = 0; i < tab.length; j++) {
+	console.log(whisper(tab[i]));
+}
+
+console.log(whisper(["PLEASE STOP SHOUTING.", "MA'AM, this is a Wendy's!"]));
+//Si simple str
+
+function whisperr(str) {
+	let newstr = str.toLowerCase();
+    if (newstr.endsWith('!')) // Ou if newstr[newstr.length - 1] === '!';
+  		newstr = newstr.slice(0, [-1]);
+	return "shh... " + newstr;
+}
+
+console.log(whisperr("PLEASE STOP SHOUTING."));
+console.log(whisperr("MA'AM, this is a Wendy's!"));
